7d198b410e76cea3a9ee8397fa0fbe424fdade64
chinoshuta <chinoshuta@gmail.com>
2025-01-05 13:46:06 +0900

    ログイン時のリダイレクトを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { getServerSession } from "next-auth";
 import { GoogleButton } from "./_components/Button/GoogleButton";
 import styles from "./page.module.css";
+import { authOptions } from "./utils/auth/config";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  // const session = await getServerSession(authOptions);
-  // console.log(session ?? "unauth");
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/dashboard");
+  }
   return (
     <section className={styles.wrapper}>
       <h1 className={styles.title}>フリーランスのための勤怠管理</h1>
