ed49b9baf06e532a4a85a7f9381ddc27025d1645
chinoshuta <chinoshuta@gmail.com>
2025-01-27 08:45:10 +0900

    Skipping auto-scroll behavior warning修正

--- app/_components/Modal/Modal.tsx
+++ app/_components/Modal/Modal.tsx
@@ -9,9 +9,11 @@ type Props = {
 export const Modal: React.FC<Props> = ({ children }) => {
   const router = useRouter();
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.layer} onClick={() => router.back()} />
-      <div className={styles.contents}>{children}</div>
+    <div>
+      <div className={styles.wrapper}>
+        <div className={styles.layer} onClick={() => router.back()} />
+        <div className={styles.contents}>{children}</div>
+      </div>
     </div>
   );
 };
